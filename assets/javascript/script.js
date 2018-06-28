3522f05627b4c93c92e064f8b9d6f56abf94f866
rusomatroso <evgeny23kl@gmail.com>
2018-06-28 13:00:06 -0400

    bonus added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
-var animals = ["crocodile", "elephant", "leopard", "puma", "cat", "eagle", "coyote", "polar bear", "panther", "komodo dragon", "wolverine", "grizly bear", "tiger", "moose"];
-
+var animals = ["crocodile", "elephant", "leopard", "shark", "cat", "eagle", "coyote", "polar bear", "panther", "komodo dragon", "monkey", "grizly bear", "tiger", "moose", "mouse"];
+var offset = 0;
 $('#showExtra10').hide();
 
 function create_buttons() {
@@ -15,25 +15,25 @@ function create_buttons() {
     $('.btn-animal').on('click', function () {
         var animalName = $(this).text();
         getAnimalGifs(animalName);
+
+        sessionStorage.clear();
+        sessionStorage.setItem("currentAnimal", animalName);
     });
 }
 
+create_buttons();
 
 function getAnimalGifs(animalName) {
 
     var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=QMJHQpZvW4PuvNmC9rJ5adhY2DgktK5b&limit=10";
 
-
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
-        printAnimalGifs(response.data);
+        printAnimalGifs(response.data, false);
     });
 }
-
-
-create_buttons();
 
 $("#addAnimal").on("click", function (event) {
     var newAnimal = $("#newAnimal").val().trim();
@@ -43,14 +43,38 @@ $("#addAnimal").on("click", function (event) {
 
 
 
-function printAnimalGifs(animals2) {
-    $('#images').empty();
+$("#showExtra10").on("click", function (event) {
+    offset = offset+10;
+    var currentAnimal = sessionStorage.getItem("currentAnimal");
+    var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + currentAnimal + "&api_key=QMJHQpZvW4PuvNmC9rJ5adhY2DgktK5b&limit=10&offset=" + offset;
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        printAnimalGifs(response.data, true);
+    });
+
+});
+
+
+function printAnimalGifs(animals2, prepend) {
+    if(prepend === false) {
+        $('#images').empty();
+    }
     if (animals2.length < 1) {
         $('#images').html('<div class="alert alert-danger">No gifs of this animal have been found</div>');
         return;
     }
     $.each(animals2, function (index, gif) {
-        $('#images').prepend('<img src="' + gif.images.downsized_still.url + '" data-still="' + gif.images.downsized_still.url + '" data-state="still" class="img-responsive img-animal" data-animated="' + gif.images.downsized.url + '">');
+        $('#images').prepend(
+        '<div class="card text-center"  style="width: 12rem;">\n' +
+            '  <img class="img-responsive img-animal crd-img"  src="' + gif.images.downsized_still.url + '" data-still="' + gif.images.downsized_still.url + '" data-state="still" data-animated="' + gif.images.downsized.url + '">\n' +
+            '  <div class="card-body">\n' +
+            '    <div class="card-text">Name</div>\n' +
+            '    <p class="mb-2"><a href="' + gif.images.original.url + '" target="_blank" class="btn btn-secondary btn-sm">download</a></p>\n' +
+            '  </div>\n' +
+            '</div>')
     });
 
     $('.img-animal').on('click', function () {
@@ -64,6 +88,6 @@ function printAnimalGifs(animals2) {
         }
 
     });
-
+    $('#showExtra10').show();
 }
 
